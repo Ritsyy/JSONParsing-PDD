9499fb52ab543f9b2ca4351aa79e9c05ad0411b4
richa rupela <richarupela95@gmail.com>
2017-01-05 20:01:20 +0530

    JSON data cleaned up and some minor fixes

--- parse.py
+++ parse.py
@@ -1,4 +1,7 @@
 import pprint
+import json
+import re
+import string
 f = open('data1.txt')
 workitem_types = ["Fundamental", "Experience", "Scenario", "Value Proposition"]
 workitem_started = False
@@ -7,17 +10,18 @@ wi = {}
 allowFirstAppend = False
 for row in f:
     
-    if row.startswith("Supporting:"):
-        wi['supporting'] = row.split(',')[1:]
+    if row.startswith("Supporting"):
+        wi['supporting'] = row.strip().split(',')[0:]
 
     # this needs to be at top, since we mark workitem_started later.
     if workitem_started:
-        wi['description'] = row
+        wi['description'] = row.strip()
         workitem_started = False
 
     # check new workitem beggining
     workitem_starts = any([row.startswith(item) for item in workitem_types])
     if workitem_starts:
+
         workitem_started = True
 
         if allowFirstAppend:
@@ -27,18 +31,22 @@ for row in f:
 
         wi = {}
         if row.startswith("Value Proposition"):
-            if len(row.split(' '))>2:
-                wi['id'] = row.split(' ')[2]
+            if len(row.strip().split(' '))>2:
+                wi['id'] = row.strip().split(' ')[2]
             else:
                 wi['id'] = "None"
-            wi['type'] =  " ".join((row.split(' ')[0], row.split(' ')[1]))             
+            wi['type'] =  " ".join((row.strip().split(' ')[0], row.strip().split(' ')[1]))
         else:
-            wi['type'] = row.split(' ')[0]
-            wi['id'] = row.split(' ')[1]
-            if len(row.split(' ')) > 2:
+            wi['type'] = row.strip().split(' ')[0]
+            wi['id'] = row.strip().split(' ')[1]
+            if len(row.strip().split(' ')) > 2:
                 pass
 
 # EOF, append last wi
 data.append(wi)
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(data)
+
+print json.dumps(data, ensure_ascii=False, encoding='utf8', indent=4)
+# pp = pprint.PrettyPrinter(indent=4)
+# pp.pprint(data)
+# with open('output.txt', 'w') as outfile:
+#     json.dump(data, outfile)
